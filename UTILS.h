c7037d7f08ffba2b2bbd461f76f98449ac616fae
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-07-17 00:28:47 +0200

    Create UTILS.h

---
+++ UTILS.h
@@ -0,0 +1,28 @@
+// ---- Secrets ----
+#ifndef UTILS_h
+#define UTILS_h
+#include <NTPClient.h>
+
+typedef enum
+{
+    OFF = false,
+    ON = true
+} light_state_t;
+
+typedef enum
+{
+    DAY,
+    NIGHT
+} day_time_t;
+
+typedef struct
+{
+    int id;
+    light_state_t state = OFF;
+    bool changed_state = false;
+    int port = 38899;
+    char *ip_addr[15];
+} light_t;
+
+
+#endif // UTILS_h
