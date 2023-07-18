75db8d3788577c83ed672bf1958fd7fe49f21c90
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-07-18 18:52:52 +0200

    fix

--- NightyWiZLight.ino
+++ NightyWiZLight.ino
@@ -12,18 +12,34 @@
 #include <NTPClient.h>         // get time from NTP server
 #include <time.h>              // for time_t
 #include <ArduinoJson.h>       // for parsing OpenWeatherMap API response
+#include <StreamUtils.h>       // for ReadLoggingStream the http stream
+
+/*
+// Required for LIGHT_SLEEP_T delay mode
+extern "C"
+{
+#include "user_interface.h"
+}
+extern "C"
+{
+#include "gpio.h"
+}
+*/
+
+//* ------- HERE ---------
 
 light_t lights[2] = {
     {0, OFF, 38899, false, "192.168.0.50"}, // light 0, init state, UDP port, changed_state(leave false), ip address
     {1, OFF, 38899, false, "192.168.0.51"}, // light 1, init state, UDP port, changed_state(leave false), ip address
 };
-const int LIGHTUP_TIME = 20; // seconds
-const int PIR_PIN = 12;      // PIR sensor pin
-const int BTN_PIN = 13;      // button pin
-const int SWITCH_PIN = 14;   // switch pin
-const int PIR_SWITCH_PIN = 15;   // PIR disable switch pin
+//! YOU CANNOT USE GPIO 16 BECAUSE INTERRUPTS ARE NOT SUPPORTED FOR THIS PIN
+const int LIGHTUP_TIME = 20;   // seconds
+const int PIR_PIN = 12;        // PIR sensor pin
+const int BTN_PIN = 13;        // button pin
+const int SWITCH_PIN = 14;     // switch pin
+const int PIR_SWITCH_PIN = 15; // PIR disable switch pin
 
-// --------------
+//* ------- ^^^^ --------
 
 const unsigned int LOCAL_UDP_PORT = 2444;
 const char GET_BUFFER[] = "{\"method\":\"getPilot\"}";                           // packet to get the state of a light
@@ -33,7 +49,17 @@ char packet_buffer[255];
 
 const char OWM_URL[] = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en";
 // day_time_t time = DAY;
-bool swiching_lights = false;
+// bool switching_lights = false;
+// bool motion = false;
+
+// TODO: debounce ?
+unsigned long now_millis = millis();
+unsigned long btn_last = btn_now;
+int btn_debounce = 200;
+unsigned long pir_last = pir_now;
+int pir_debounce = 200;
+unsigned long switch_last = switch_now;
+int switch_debounce = 200;
 
 WiFiClient client;
 // HTTPClient http; // set only when using OWM
@@ -46,7 +72,7 @@ NTPClient timeClient(Udp, "pool.ntp.org");
 HTTPClient http;
 void get_sunset_sunrise_time(time_t &sunrise, time_t &sunset, time_t now)
 {
-    //String json_buffer;
+    // String json_buffer;
     String server_path;
     server_path = OWM_URL;
     server_path += "&lat=";
@@ -57,8 +83,8 @@ void get_sunset_sunrise_time(time_t &sunrise, time_t &sunset, time_t now)
     server_path += OWM_API_KEY;
 
     Serial.print(F("[HTTP] begin...\n"));
-    
-    http.useHTTP10(true); // force HTTP/1.0, not 1.1, to be able to get http stream
+
+    http.useHTTP10(true);                                  // force HTTP/1.0, not 1.1, to be able to get http stream
     http.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS); // not necessary ?
 
     if (http.begin(client, server_path.c_str()))
@@ -66,7 +92,7 @@ void get_sunset_sunrise_time(time_t &sunrise, time_t &sunset, time_t now)
 
         Serial.print(F("[HTTP] GET...\n"));
         int http_response_code = http.GET();
-        //String payload = "{}";
+        // String payload = "{}";
 
         if (http_response_code > 0)
         {
@@ -77,17 +103,17 @@ void get_sunset_sunrise_time(time_t &sunrise, time_t &sunset, time_t now)
                 // payload = http.getString();
                 // Serial.println(payload);
 
-                //const size_t capacity = JSON_ARRAY_SIZE(3) + 2 * JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + 3 * JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 480;
-                //DynamicJsonDocument data(capacity);
+                // const size_t capacity = JSON_ARRAY_SIZE(3) + 2 * JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + 3 * JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 480;
+                // DynamicJsonDocument data(capacity);
                 DynamicJsonDocument data(96); // from https://arduinojson.org/v6/assistant/
-                //StaticJsonDocument<64> filter;
-                //filter["sys"] = true;
+                // StaticJsonDocument<64> filter;
+                // filter["sys"] = true;
                 StaticJsonDocument<48> filter; // from https://arduinojson.org/v6/assistant/
                 JsonObject filter_sys = filter.createNestedObject("sys");
                 filter_sys["sunrise"] = true;
                 filter_sys["sunset"] = true;
-                Serial.print(F("JSON filter: "));
-                Serial.println(filter);
+                // Serial.print(F("JSON filter: "));
+                // Serial.println(filter);
                 ReadLoggingStream loggingStream(http.getStream(), Serial); // prints stream to Serial
                 DeserializationError error = deserializeJson(data, loggingStream, DeserializationOption::Filter(filter));
                 if (error)
@@ -123,7 +149,7 @@ void get_sunset_sunrise_time(time_t &sunrise, time_t &sunset, time_t now)
         {
             Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(http_response_code).c_str());
         }
-        //json_buffer = payload;
+        // json_buffer = payload;
         http.end();
     }
     else
@@ -304,7 +330,7 @@ bool wifi_setup()
 
 bool switch_lights()
 {
-    swiching_lights = true;
+    // switching_lights = true;
     bool res = false;
 
     for (int i; i < sizeof(lights) / sizeof(lights[0]); i++)
@@ -346,14 +372,14 @@ bool switch_lights()
             res = switch_light_state(light, OFF);
         }
     }
-    swiching_lights = false;
+    // switching_lights = false;
     return res;
 }
 
 day_time_t check_night_time()
 {
     time_t now = timeClient.getEpochTime();
-    Serial.println(F("Now time: ") + (unsigned long)now);
+    Serial.println(F("Now time: ") + String(now));
     time_t sunrise;
     time_t sunset;
     get_sunset_sunrise_time(sunrise, sunset, now);
@@ -370,11 +396,15 @@ day_time_t check_night_time()
     }
 }
 
-void motion_detected_interrupt()
+void IRAM_ATTR motion_detected_interrupt()
 {
-    if (!swiching_lights || !(digitalRead(PIR_SWITCH_PIN) == LOW)) //TODO: verify second condition
+    // TODO: debounce?
+    delay(10);
+    if (digitalRead(PIR_SWITCH_PIN) == HIGH)
+    // if (!switching_lights && (digitalRead(PIR_SWITCH_PIN) == HIGH) && !motion) // doesn't have to check if switching is finished because can't interrupt during an ISR
     // prevents the lights from switching again if PIR interrupt during the delay, or if the switch is on
     {
+        // motion = true;
         timeClient.update();
         day_time_t current_time = check_night_time();
         if (current_time == NIGHT)
@@ -382,19 +412,46 @@ void motion_detected_interrupt()
             switch_lights();
         }
     }
+    /*
+    else if (motion && (digitalRead(PIR_SWITCH_PIN) == LOW))
+    {
+        motion = false;
+    }
+    */
 }
 
-void btn_interrupt()
+void IRAM_ATTR btn_interrupt()
 {
-    //same as motion_detected_interrupt() but without night check
-    if (!swiching_lights)
+    // TODO: debounce?
+    delay(10);
+    // same as motion_detected_interrupt() but without night check
+    switch_lights();
+    /*
+    if (!switching_lights) // doesn't have to check if switching is finished because can't interrupt during an ISR
     {
         switch_lights();
     }
+    */
 }
 
-void switch_interrupt()
+void IRAM_ATTR switch_interrupt()
 {
+    // TODO: debounce?
+    delay(10);
+    // TODO: ? put logic outside of interrupt func?
+    /*
+    now_millis = millis();
+    if (now_millis - switch_last < switch_debounce)
+    {
+        return;
+    } else
+    {
+        switch_last = now_millis;
+        switch_interrupted = true;
+        finished = false;
+    }
+    */
+
     if (digitalRead(SWITCH_PIN) == LOW)
     {
         Serial.println(F("Switching lights ON"));
@@ -420,25 +477,50 @@ void switch_interrupt()
 void setup()
 {
     Serial.begin(115200);
-    pinMode(PIR_PIN, INPUT); // TODO: pullup or pulldown?
+    Serial.println(F("setup() start"));
+    delay(200);
+
+    pinMode(PIR_PIN, INPUT); // TODO: ? use an hardware pulldown resistor for PIR?
     pinMode(BTN_PIN, INPUT_PULLUP);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(SWITCH_PIN, INPUT_PULLUP);
     pinMode(PIR_SWITCH_PIN, INPUT_PULLUP);
-    wifi_setup();
 
-    Udp.begin(LOCAL_UDP_PORT);
+    wifi_setup();
+    // wifi_set_sleep_type(LIGHT_SLEEP_T);
+    Udp.begin(LOCAL_UDP_PORT); // begin in switch_lights() or switch_light_state() instead?
     timeClient.begin();
 
-    attachInterrupt(digitalPinToInterrupt(PIR_PIN), motion_detected_interrupt, CHANGE); // TODO: rising or falling?
+    attachInterrupt(digitalPinToInterrupt(PIR_PIN), motion_detected_interrupt, CHANGE);
     attachInterrupt(digitalPinToInterrupt(BTN_PIN), btn_interrupt, FALLING);
     attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), switch_interrupt, CHANGE);
+    Serial.println(F("Attached interrupts"));
+
+    Serial.println(F("setup() done"));
 }
 
 void loop()
 {
-    // sould never get here?
+    // wifi_setup(); // in loop() because we use light sleep
+    // TODO: sould or should not get here?
     timeClient.update();
-    delay(2000);
-    Serial.println(F("Looping..."));
+    // Serial.println(F("Sleeping..."));
+    // light_sleep(); // enter light sleep mode
+    // continues here once woken up
 }
+
+// not used
+#ifdef LIGHT_SLEEP_ENABLED
+void light_sleep()
+{
+    wifi_station_disconnect();
+    wifi_set_opmode_current(NULL_MODE);
+    wifi_fpm_set_sleep_type(LIGHT_SLEEP_T); // set sleep type, the above posters wifi_set_sleep_type() doesn't seem to work for me although it did let me compile and upload with no errors
+    wifi_set_sleep_type(LIGHT_SLEEP_T);     // doesn't do anything ?
+    wifi_fpm_open();                        // Enables force sleep
+    wifi_fpm_set_wakeup_cb(light_sleep_wakeup_callback);
+    gpio_pin_wakeup_enable(GPIO_ID_PIN(2), GPIO_PIN_INTR_LOLEVEL); // GPIO_ID_PIN(2) corresponds to GPIO2 on ESP8266-01 , GPIO_PIN_INTR_LOLEVEL for a logic low, can also do other interrupts, see gpio.h above
+    // wifi_fpm_do_sleep(0xFFFFFFF);                                  // Sleep for longest possible time
+    wifi_fpm_do_sleep(FPM_SLEEP_MAX_TIME); // Sleep for longest possible time
+}
+#endif
